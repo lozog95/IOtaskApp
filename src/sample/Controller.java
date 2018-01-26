f20b2526b37b31c61c756ecc6f5367ba304c5265
lozog95 <lozog95@gmail.com>
2018-01-26 23:25:22 +0100

    login screen created

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,20 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class Controller {
+    public PasswordField passwordField;
+    public TextField loginField;
+    public Text actionTarget;
+
+    public void handleSubmitButtonAction(ActionEvent actionEvent) {
+        if(loginField.getText().equals("") || passwordField.equals("")){
+            actionTarget.setText("puste");
+        }else{
+            actionTarget.setText("nie puste!");
+        }
+    }
 }
