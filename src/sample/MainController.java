e9cab53947e0b32e5195e11fbb2882555578682a
lozog95 <lozog95@gmail.com>
2018-01-31 21:54:25 +0100

    loading to tableview and add button

--- src/sample/MainController.java
+++ src/sample/MainController.java
@@ -4,13 +4,14 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.util.Random;
 
 public class MainController {
 
 
-    public TableView tableView;
+    public TableView<Task> tableView;
     public TextField titleField;
     public TextField descField;
     public TextField estField;
@@ -18,17 +19,32 @@ public class MainController {
     public Button addBtn;
     public ObservableList<Task> taskObservableList;
     public void initialize(){
-        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        taskObservableList=FXCollections.observableArrayList();
         ownerField.setItems(FXCollections.observableArrayList(
                 "lozog", "root", "sample", "kamil") //to add users dynamically from db.
         );
+
+        Task task = new Task(1, "task", 3, new User("lozog"), "ddd");
+        taskObservableList.add(task);
+        TableColumn title = new TableColumn("Task");
+
+        title.setCellValueFactory(
+                new PropertyValueFactory<>("description"));
+
+
+        tableView.setItems(taskObservableList);
+        tableView.getColumns().add(title);
+        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
     }
 
 
     public void addButton(ActionEvent actionEvent) {
         int taskId = new Random().nextInt();
+       // User user = new User(taskId, titleField.getText(), estField.getText(),  );
         Task task = new Task(taskId,titleField.getText(),  Integer.parseInt(estField.getText()), new User(ownerField.selectionModelProperty().toString()), "Task "+taskId+" of "+ownerField.selectionModelProperty().toString());
-        taskObservableList.add(1, task);
-
+        taskObservableList.add(task);
+        System.out.println(task.getDescription());
+        tableView.refresh();
     }
 }
