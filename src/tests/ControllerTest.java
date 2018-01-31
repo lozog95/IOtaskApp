838e7e293a97ef2430633b7cbb1df64e4bdaaad7
Kamil Kozyra <kamilkozyra@gmail.com>
2018-01-31 22:53:30 +0100

    added test classes

---
+++ src/tests/ControllerTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import sample.Controller;
+
+public class ControllerTest {
+    @Test
+    public void firstTest() {
+        Assert.assertTrue(true);
+    }
+}
---
+++ src/tests/MainControllerTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import sample.MainController;
+
+public class MainControllerTest {
+    @Test
+    public void firstTest() {
+        Assert.assertTrue(true);
+    }
+}
