15023d741d838c266b917c9e3f7079f7064046c6
Marcin Mirzynski <mmirzyns@pc40.home>
2018-02-01 20:41:12 +0100

    Implemented ExportCSV function

--- src/sample/MainController.java
+++ src/sample/MainController.java
@@ -12,6 +12,8 @@ import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -205,6 +207,54 @@ public class MainController {
         result.add(curVal.toString());
 
         return result;
+    }
+
+    public static void writeLine(Writer w, List<String> values) throws IOException {
+        writeLine(w, values, DEFAULT_SEPARATOR, ' ');
+    }
+
+    public static void writeLine(Writer w, List<String> values, char separators) throws IOException {
+        writeLine(w, values, separators, ' ');
+    }
+
+    //https://tools.ietf.org/html/rfc4180
+    private static String followCVSformat(String value) {
+
+        String result = value;
+        if (result.contains("\"")) {
+            result = result.replace("\"", "\"\"");
+        }
+        return result;
+
+    }
+
+    public static void writeLine(Writer w, List<String> values, char separators, char customQuote) throws IOException {
+
+        boolean first = true;
+
+        //default customQuote is empty
+
+        if (separators == ' ') {
+            separators = DEFAULT_SEPARATOR;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String value : values) {
+            if (!first) {
+                sb.append(separators);
+            }
+            if (customQuote == ' ') {
+                sb.append(followCVSformat(value));
+            } else {
+                sb.append(customQuote).append(followCVSformat(value)).append(customQuote);
+            }
+
+            first = false;
+        }
+        sb.append("\n");
+        w.append(sb.toString());
+
+
     }
 
 
